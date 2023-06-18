a3658febda5cb4e3e7305837c31f1502520abefb
tkdgks7036 <133713235+tkdgks7036@users.noreply.github.com>
2023-06-18 11:27:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,18 +1,3 @@
 function solution(s) {
-    let str = s.split(' ');
-    let change = [];
-
-    for (let i = 0; i < str.length; i++) {
-        let word = str[i].split('').map((a, idx) => {
-            if (idx % 2 === 0) {
-                return a.toUpperCase();
-            }
-            else {
-                return a.toLowerCase();
-            }
-        }).join('');
-        change.push(word);
-    };
-
-    return change.join(' ');
+    return s.split(' ').map((a, idx) => (idx % 2 === 0) ? a.toUpperCase() : a.toLowerCase()).join(' ');
 }
