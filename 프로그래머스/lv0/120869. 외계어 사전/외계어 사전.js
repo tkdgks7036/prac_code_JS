cefa4029eb77d6a08641a653d385a36a48a10de0
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-06-20 01:14:39 +0900

    [level 0] Title: 외계어 사전, Time: 2.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,10 +1,27 @@
+// function solution(spell, dic) {
+//     let match_result = 2;
+
+//     dic.map(x => {
+//         let all_match = spell.every(a => x.includes(a));
+//         if (all_match) return match_result = 1;
+//     });
+
+//     return match_result;
+// };
+
 function solution(spell, dic) {
-    let match_result = 2;
+    var answer = 0;
+    let spe = spell.sort().join(''); // 'dxz'
+    let ddic = [];
+    // let mat2;
 
-    dic.map(x => {
-        let all_match = spell.every(a => x.includes(a));
-        if (all_match) return match_result = 1;
-    });
+    for (let i = 0; i < dic.length; i++) {
+        dic[i] = [...dic[i]].sort().join('');
+        ddic.push(dic[i]);
+    }; // [ 'def', 'dww', 'dxz', 'aelovw' ]
 
-    return match_result;
-};
+    mat2 = ddic.includes(spe);
+    console.log(mat2);
+
+    return mat2 ? 1 : 2
+}
