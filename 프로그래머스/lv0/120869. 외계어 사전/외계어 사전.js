33d2f402360609f1c9827c5bd57a5d436fa95b86
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-06-20 01:29:30 +0900

    Update 외계어 사전.js

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,27 +1,10 @@
-// function solution(spell, dic) {
-//     let match_result = 2;
-
-//     dic.map(x => {
-//         let all_match = spell.every(a => x.includes(a));
-//         if (all_match) return match_result = 1;
-//     });
-
-//     return match_result;
-// };
-
 function solution(spell, dic) {
-    var answer = 0;
-    let spe = spell.sort().join(''); // 'dxz'
-    let ddic = [];
-    // let mat2;
+    let match_result = 2;
 
-    for (let i = 0; i < dic.length; i++) {
-        dic[i] = [...dic[i]].sort().join('');
-        ddic.push(dic[i]);
-    }; // [ 'def', 'dww', 'dxz', 'aelovw' ]
+    dic.map(x => {
+        let all_match = spell.every(a => x.includes(a));
+        if (all_match) return match_result = 1;
+    });
 
-    mat2 = ddic.includes(spe);
-    console.log(mat2);
-
-    return mat2 ? 1 : 2
-}
+    return match_result;
+};
