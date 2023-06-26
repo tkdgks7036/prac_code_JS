361d53008c024867b80a1715b70d0d1b1866275e
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-06-26 23:33:07 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,22 @@
+function solution(my_string) {
+    let arr_full = Array(52).fill(0);
+    let arr_alpha = ['A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f',
+        'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r',
+        's', 't', 'u', 'v', 'w', 'x',
+        'y', 'z'
+    ];
+
+    let arr = my_string.split('').map(x => arr_alpha.indexOf(x));
+
+    for (let i of arr) {
+        arr_full[i] += 1;
+    };
+
+    return arr_full;
+};
