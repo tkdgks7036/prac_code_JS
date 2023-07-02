7fccb43c708c1c440b5c808bc9a0236add74b836
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-07-02 19:32:33 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.58 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 50 && arr[i] % 2 === 0) arr[i] /= 2;
+        else if (arr[i] < 50 && arr[i] % 2 === 1) arr[i] *= 2;
+    };
+
+    return arr;
+};
