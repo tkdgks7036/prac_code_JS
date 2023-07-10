78bde975fac96309c0c45ad1b8720a17ef09b94b
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-07-10 22:36:06 +0900

    주석내용 추가

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,17 +1,34 @@
+let babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"];
+
 function solution(babbling) {
 
-    return babbling.map(x => {
+    // Todo => map
+    return babbling.map(x => { // 'ayaye'
         let speak = ["aya", "ye", "woo", "ma"];
 
         for (let i = 0; i < speak.length; i++) {
+            // 발음할 수 있는 값을 기준으로 split
+            // join(' ') 을 한 이유?
+            // join('') 을 하게되면 공백없이 붙여지기 때문에
+            // 값이 합쳐지면서 불가능한 발음이 가능하게 되어버리는 경우를 방지하기 위함
+
+            // ex. wyeoo => ye 를 기준으로 split => ['w', '', 'oo]
+            // join('') 을 하게되면 woo 가 되어버림
             x = x.split(speak[i]).join(' ');
+
+            // speak[0] => 'aya' => 'ayaye'.split('aya') => ['', 'ya'] => [' ye']
+            // speak[1] => ' ye' => ' ye.split('ye') => ['', '']
         };
 
         return x;
     })
-        
+    
+    // Todo => filter
     .filter(x => {
         let pattern = /[a-z]/;
+        // a-z 중 하나라도 포함이 되는지 확인하기 위함
         return !pattern.test(x)
     }).length;
 };
+
+console.log(solution(babbling));
