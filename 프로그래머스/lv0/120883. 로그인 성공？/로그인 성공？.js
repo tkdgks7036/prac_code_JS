c8299bcc0eb2f471b1e1cdcafd5595f877dc84df
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-07-12 22:28:53 +0900

    [level 0] Title: 로그인 성공?, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,9 +1,14 @@
 function solution(id_pw, db) {
     for (let i = 0; i < db.length; i++) {
+
+        // db[i] 번째의 값을 각각 id, pw 로 구조분해할당
         [id, pw] = db[i];
+
+        // id_pw[0] : id & id_pw[1] : pw 로 항상 고정인 점을 이용
         if (id === id_pw[0] && pw === id_pw[1]) return 'login';
         else if (id === id_pw[0] && pw !== id_pw[1]) return 'wrong pw';
     };
 
+    // id, pw 모두 맞지 않을 경우를 기본 값으로 두었다.
     return 'fail'
 };
