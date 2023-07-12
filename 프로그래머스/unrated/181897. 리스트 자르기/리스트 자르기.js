03e253a3a83e1e0ae3ed3f1fd24c8320e26070ea
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-07-12 23:10:45 +0900

    [unrated] Title: 리스트 자르기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,14 @@
+function solution(n, slicer, num_list) {
+    let [a, b, c] = [...slicer];
+
+    switch (n) {
+        case 1:
+            return num_list.slice(0, b + 1);
+        case 2:
+            return num_list.slice(a);
+        case 3:
+            return num_list.slice(a, b + 1);
+        case 4:
+            return num_list.slice(a, b + 1).filter((_, idx) => idx % c === 0);
+    };
+};
