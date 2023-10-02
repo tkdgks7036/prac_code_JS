2a6f8ca8633716c51a37156a1c4ddffd88b176b9
Luna <133713235+tkdgks7036@users.noreply.github.com>
2023-10-02 15:36:43 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,6 +1,11 @@
 function solution(A, B) {
+  let answer = 0;
   A.sort((a, b) => a - b);
   B.sort((a, b) => b - a);
 
-  return A.reduce((acc, cur, idx) => (acc += cur * B[idx]), 0);
+  for (let i = 0; i < A.length; i++) {
+    answer += A[i] * B[i];
+  }
+
+  return answer;
 }
